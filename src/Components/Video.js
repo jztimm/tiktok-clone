5f2e5b2f42c5c8b792a66d633dd085d628022841
jztimm <jztimm@gmail.com>
2021-02-06 14:30:23 -0500

    Input dummy data for video

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -4,7 +4,11 @@ import "../Styles./Video.css"
 function Video() {
   return (
     <div className="video">
-      
+      <video src="https://www.tiktok.com/@thehattiemae/video/6904757382992219398"></video>
+
+      {/* Video footer */}
+      {/* Video Sidebar */}
+
     </div>
   )
 }
