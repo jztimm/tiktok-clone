e552f7a21511bc504ef56853168cf123690fc33b
jztimm <jztimm@gmail.com>
2021-02-09 18:19:55 -0500

    updated CSS of app and video. Finished scroll snap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,16 @@ import Video from './Components/Video';
 function App() {
   return (
     <div className="App">
-      <h1>Full Stack MERN Tik Tok Clone</h1>
-      <Video />
-
-      {/* app container */}
-        {/* videos */}
-
       <div className="app_videos">
         <Video />
         <Video />
       </div>
 
-        {/* 
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+      {/* <Video /> */}
+
 
 
     </div>
